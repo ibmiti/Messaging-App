1ab51a14f421517780ab925ce8a0ff4bc8950cd3
Dimitri mcDaniel <ibmiti@Dimitris-MacBook-Air.local>
2018-12-15 05:00:16 -0500

    committing javascript and css styling changes

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,3 +1,12 @@
-var messages = document.getElementById("messages")
-var textbox = document.getElementById("messages")
-var button = document.getElementById("messages")
+var messages = document.getElementById("messages");
+var textbox = document.getElementById("textbox");
+var button = document.getElementById("button");
+
+
+button.addEventListener("click",
+  function(){
+    var newMessage = document.createElement("li"); // created new html element on page
+    newMessage.innerHTML = textbox.value; // and then added text to new element, and what ever value that would be inputted into text box
+    messages.appendChild(newMessage); // and then appended the li item
+    textbox.value = ""; // this clears the text box into an empty str until it is filled with another value and then appended
+  });
